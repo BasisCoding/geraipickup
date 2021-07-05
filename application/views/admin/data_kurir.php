40760f07411863557de64dddd1344e9326bcec5e
basiscoding20 <basiscoding20@gmail.com>
2021-07-05 22:32:32 +0700

    Add Input Password Kurir

--- application/views/admin/data_kurir.php
+++ application/views/admin/data_kurir.php
@@ -61,6 +61,11 @@
                                     </div>
 
                                     <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="text" name="password" class="form-control" required="">
+                                    </div>
+
+                                    <div class="form-group">
                                         <label>Email</label>
                                         <input type="email" name="email" class="form-control" required="">
                                     </div>
